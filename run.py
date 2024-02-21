90e4432cf8ce52c93e09d503be6bea033704b11b
Cam Wheatley <cameron_2000@live.co.uk>
2024-02-21 17:07:14 +0000

    add employee class with first employee

--- run.py
+++ run.py
@@ -15,5 +15,20 @@ SHEET = GSPREAD_CLIENT.open('railway_inn_employees')
 employee_page = SHEET.worksheet('Employees')
 
 data = employee_page.get_all_values()
+row = data[-1]
 
-print(data)
+class Employee():
+
+    def __init__(self, first_name, last_name, employee_no, contract_hours, wage):
+       self.first_name = first_name
+       self.last_name = last_name
+       self.employee_no = employee_no
+       self.contract_hours = contract_hours
+       self.wage = wage
+    
+    def printList(self):
+        list = [self.first_name, self.last_name, self.employee_no, self.contract_hours, self.wage]
+        print(list)
+
+Hannah = Employee('Hannah', 'Obrien', 2, 40, '£15')
+Hannah.printList()
